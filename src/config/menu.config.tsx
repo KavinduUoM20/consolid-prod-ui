1a6fd5ad35fead4cbea2dd17135fcd410ad818ec
_im'kavindu <65580465+KavinduUoM20@users.noreply.github.com>
2025-07-23 09:28:48 +0530

    fix: menu configs changed

--- src/config/menu.config.tsx
+++ src/config/menu.config.tsx
@@ -1722,7 +1722,7 @@ export const MENU_ROOT: MenuConfig = [
     childrenIndex: 4,
   },
   {
-    title: 'DocIQ - Extractions',
+    title: 'Material Extraction',
     icon: FileChartColumn,
     rootPath: '/dociq/',
     path: 'dociq/home',
